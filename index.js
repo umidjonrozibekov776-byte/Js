0144038c9b1a4863d1387ccda542b3fcc6d1b5a7
Umidjon <umidjonrozibekov@gmail.com>
2025-10-04 10:58:40 +0500

    Js haqida ma'lumotlar

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+console.log("Men JSda birinchi kodimni yozdim.")
+
+
+var name = "Umidjon"
+
+let age = 17;
+
+let a = "50";
+let b = "50";
+let ab = a + b;
+console.log(ab);
+
+
+console.log(typeof ism);
+console.log(typeof ism);
+console.log(typeof age);
