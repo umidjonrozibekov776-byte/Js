9abb1f222771268efb5913a22da70d1a0d519159
Umidjon <umidjonrozibekov@gmail.com>
2025-10-04 15:38:09 +0500

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-
-
-console.log("Men JSda birinchi kodimni yozdim.")
-
-
 var name = "Umidjon"
 
-let age = 17;
 
-let a = "50";
-let b = "50";
-let ab = a + b;
-console.log(ab);
+let age = "17"
 
-
-console.log(typeof ism);
-console.log(typeof ism);
-console.log(typeof age);
